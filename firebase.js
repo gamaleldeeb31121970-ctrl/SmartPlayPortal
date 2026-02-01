428a23b37ed9e47eb75f4aeac3b0cc3b4460e092
gamaleldeeb31121970-ctrl <gamaleldeeb31121970@gmail.com>
2026-02-01 20:45:47 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,19 @@
 
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAwyE1ZD4hc7E_T2HGcSiUkW2vkkGTMDWw",
+  authDomain: "smartplayportal-2d950.firebaseapp.com",
+  projectId: "smartplayportal-2d950",
+  storageBucket: "smartplayportal-2d950.firebasestorage.app",
+  messagingSenderId: "990435573818",
+  appId: "1:990435573818:web:b052b7633dd3d131c3b503",
+  measurementId: "G-QG707935DM"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export default app;
